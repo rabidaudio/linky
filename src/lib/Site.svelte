696747c61cc15936939df70c7095d6aaf42acc62
Julien <julien@rabidaudio.com>
2025-05-22 17:32:34 -0400

    drag reorder

--- src/lib/Site.svelte
+++ src/lib/Site.svelte
@@ -1,22 +1,34 @@
 <script>
+	import {dndzone} from 'svelte-dnd-action'
+	import {flip} from 'svelte/animate'
+
     import Link from "./Link.svelte"
 
-    export let links = [
-      {name: "Stolen Velour", value: "https://stolenvelour.music", type: "url" },
-      {name: "BlueSky", value: "https://bsky.app/profile/okaysure.cool", type: "url" },
-      {name: "Instagram", value: "https://www.instagram.com/okay_sure_cool", type: "url" },
-      {name: "Discord", value: "okay_sure_cool", type: "copyable" },
-      {name: "Blog", value: "https://rabid.audio", type: "url" },
-      {name: "Bandcamp", value: "https://bandcamp.com/rabidaudio", type: "url" },
-      {name: "Last.FM", value: "https://last.fm/user/rabidaudio", type: "url" },
-      {name: "Songkick (concert history)", value: "https://www.songkick.com/users/rabidaudio/gigography", type: "url" },
-      {name: "Twitch", value: "https://twitch.tv/okay_sure_cool", type: "url" },
-      {name: "Discogs", value: "https://www.discogs.com/user/okay_sure_cool", type: "url" },
-      {name: "Letterboxd", value: "https://letterboxd.com/okay_sure_cool", type: "url" },
-      {name: "Mastodon", value: "https://hachyderm.io/@okay_sure_cool", type: "url" },
-      {name: "BeReal", value: "https://bere.al/okay_sure_cool", type: "url" },
-      {name: "Nintendo friend code", value: "SW-5237-4139-3316", type: "copyable" },
+    const flipDurationMs = 200
+
+
+	export let links = [
+        {id: 1, name: "Stolen Velour", value: "https://stolenvelour.music", type: "url" },
+        {id: 2, name: "BlueSky", value: "https://bsky.app/profile/okaysure.cool", type: "url" },
+        {id: 3, name: "Instagram", value: "https://www.instagram.com/okay_sure_cool", type: "url" },
+        {id: 4, name: "Discord", value: "okay_sure_cool", type: "copyable" },
+        {id: 5, name: "Blog", value: "https://rabid.audio", type: "url" },
+        {id: 6, name: "Bandcamp", value: "https://bandcamp.com/rabidaudio", type: "url" },
+        {id: 7, name: "Last.FM", value: "https://last.fm/user/rabidaudio", type: "url" },
+        {id: 8, name: "Songkick (concert history)", value: "https://www.songkick.com/users/rabidaudio/gigography", type: "url" },
+        {id: 9, name: "Twitch", value: "https://twitch.tv/okay_sure_cool", type: "url" },
+        {id: 10, name: "Discogs", value: "https://www.discogs.com/user/okay_sure_cool", type: "url" },
+        {id: 11, name: "Letterboxd", value: "https://letterboxd.com/okay_sure_cool", type: "url" },
+        {id: 12, name: "Mastodon", value: "https://hachyderm.io/@okay_sure_cool", type: "url" },
+        {id: 13, name: "BeReal", value: "https://bere.al/okay_sure_cool", type: "url" },
+        {id: 14, name: "Nintendo friend code", value: "SW-5237-4139-3316", type: "copyable" },
     ]
+
+    function handleSort(e) {
+		links = e.detail.items;
+	}
+
+    $: console.log(links.map(l => l.name).join(", "))
 </script>
 
 <style>
@@ -26,8 +38,9 @@
 }
 </style>
 
-<div class="link-container">
-    {#each links as link }
+<div class="link-container" use:dndzone={{items: links, dropTargetStyle: {outline: 'var(--bulma-info) solid 2px'} }}
+on:consider={handleSort} on:finalize={handleSort}>
+	{#each links as link (link.id)}
         <Link {...link} />
-    {/each}
+	{/each}
 </div>
